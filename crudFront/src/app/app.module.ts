2f553ee23dd0c3c99662b89b66aa619602793651
DeiMoS <nagib.86@gmail.com>
2023-02-25 13:48:45 +0100

    service implementado

--- crudFront/src/app/app.module.ts
+++ crudFront/src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-//External
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToastrModule } from 'ngx-toastr';
 import { ListaProductoComponent } from './producto/lista-producto.component';
 import { DetalleProductoComponent } from './producto/detalle-producto.component';
 import { NuevoProductoComponent } from './producto/nuevo-producto.component';
 import { EditarProductoComponent } from './producto/editar-producto.component';
+
+import {HttpClientModule} from '@angular/common/http'
+import {FormsModule} from '@angular/forms'
+//External
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
 
 @NgModule({
   declarations: [
@@ -24,6 +28,8 @@ import { EditarProductoComponent } from './producto/editar-producto.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot(), 
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- crudFront/src/app/service/producto.service.ts
+++ crudFront/src/app/service/producto.service.ts
@@ -1,9 +1,45 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Producto } from '../models/producto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
 
-  constructor() { }
+  //El service es el que se conecta, tengo que pasarle la url a la que tiene que conectar
+  productoUrl = 'http://localhost:8080/producto/';
+  constructor(private httpClient: HttpClient) { }
+
+  //metodos del backend
+
+  //Metodo para listar los productos, devuelve un Observable porque así lo definimos en el backend y un array de Productos por eso los []
+  public lista(): Observable<Producto[]>{
+    return this.httpClient.get<Producto[]>(this.productoUrl + 'lista');
+  }
+
+  //Este metodo como necesita parámetro, para pasarlo por la Url ponemos comillas `` y el parámetro a pasar entre ${}
+  public detail(id: number): Observable<Producto>{
+    return this.httpClient.get<Producto>(this.productoUrl + `detail/${id}`);
+  }
+
+  public detailName(nombre: string): Observable<Producto>{
+    return this.httpClient.get<Producto>(this.productoUrl + `detailname/${nombre}`);
+  }
+
+  //Como este método no devuelve nada, así lo especificamos en el backend, se pone any en el tipo devuelto
+  //En el método post hay un requestbody, así lo especificamos en el backend, entonces hay que decirle lo que le vamos a enviar, en este caso un {producto}
+  //Si utilizo un post y no envio un requestbody, se le pasarían {} (vacio)
+  public save(producto: Producto): Observable<any>{
+    return this.httpClient.post<any>(this.productoUrl + 'create', {producto});
+  }
+
+  public update(id: number ,producto: Producto): Observable<any>{
+    return this.httpClient.put<any>(this.productoUrl + `update/${id}`, {producto});
+  }
+
+  public delete(id: number):Observable<any>{
+    return this.httpClient.delete<any>(this.productoUrl + `delete/${id}`);
+  }
 }
