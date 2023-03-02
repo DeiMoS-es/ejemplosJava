db2a556fb0ca13cf4597c1ae1a6bfde449a4d4ec
DeiMoS <nagib.86@gmail.com>
2023-03-02 11:39:40 +0100

    creadas las clases principales

--- 001_crud_jwt/src/main/java/com/example/crud/security/entity/Usuario.java
+++ 001_crud_jwt/src/main/java/com/example/crud/security/entity/Usuario.java
@@ -1,5 +1,102 @@
 package com.example.crud.security.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
 public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@NotNull
+	private String nombre;
+	@NotNull
+	@Column(unique = true)
+	private String nombreUsuario;
+	@NotNull
+	private String email;
+	@NotNull
+	private String password;
+	//Aqui tenemos una relación muchos a muchos
+	//Para implementar eso se crea una tabla intermedia con los ids de usuarios y roles
+	//En la tabla usuario_rol hay un campo usuario_id que hace referencia a la tabla Usuario
+	@NotNull
+	@ManyToMany
+	@JoinTable(name = "usuario_rol", joinColumns = @JoinColumn(name = "usuario_id"),
+	inverseJoinColumns = @JoinColumn(name = "rol_id"))
+	private Set<Rol> roles = new HashSet<>();
+	
+	
+	public Usuario() {
+		super();
+	}
+
+	public Usuario(@NotNull String nombre, @NotNull String nombreUsuario, @NotNull String email,
+			@NotNull String password) {
+		super();
+		this.nombre = nombre;
+		this.nombreUsuario = nombreUsuario;
+		this.email = email;
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Set<Rol> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Rol> roles) {
+		this.roles = roles;
+	}
 
 }
--- 001_crud_jwt/src/main/java/com/example/crud/security/enums/RolNombre.java
+++ 001_crud_jwt/src/main/java/com/example/crud/security/enums/RolNombre.java
@@ -1,5 +1,7 @@
 package com.example.crud.security.enums;
 
 public enum RolNombre {
+	//Tipos de roles
+	ROLE_ADMIN, ROLE_USER
 
 }
